479099364b9f661d3881db0201acc3253c455577
Neoncodes1 <neoncodes1@gmail.com>
2025-07-22 14:40:59 +0100

    Update and rename package.json to index.js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// AlphaSock basic export
+module.exports = {
+    connect: () => {
+        console.log("AlphaSock JS client connected. Building WebSocket layer...");
+        // Placeholder for WebSocket logic
+    }
+};
